20ad4853ed2973dbdb04e2db915007b254173585
Cgparfait <152601889+Cgparfait@users.noreply.github.com>
2025-03-10 18:16:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // set view engine
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 
 
