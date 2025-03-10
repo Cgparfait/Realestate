81bbc89f191ef563c5a5c34c955c9aab03037ea7
Cgparfait <152601889+Cgparfait@users.noreply.github.com>
2025-03-10 18:04:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 // set view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.set("views", path.join(__dirname, "views"));
 
 
 // body parser middleware
