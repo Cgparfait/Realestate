32e9e9aa90e84a5511e86cc760a09064c949e6c0
cgparfait <rparfait720@gmail.com>
2025-03-24 12:45:34 -0400

    sneakview

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -27,6 +27,7 @@ async function getOrganicSearchMetrics() {
     // Replace with your GA4 property ID
     const PROPERTY_ID = process.env.ANALYTICS_PROPERTY_ID;
 
+    generate_analytics_api_key_file()
     const authClient = new JWT({
         keyFile: KEY_FILE_PATH,
         scopes: 'https://www.googleapis.com/auth/analytics.readonly'
@@ -70,6 +71,33 @@ async function getOrganicSearchMetrics() {
     console.log(organic_google_search_click_through_rate)
 
     return { organic_google_search_clicks, organic_google_search_impressions, organic_google_search_click_through_rate }
+}
+
+function generate_analytics_api_key_file() {
+    const analytics_key_file_name = process.env.ANALYTICS_API_KEY_FILE_NAME
+    const analytics_key_file_data = process.env.ANALYTICS_API_KEY_FILE
+
+    secret_folder_name = process.env.SECRET_FOLDER_NAME
+    filePath = path.join(secret_folder_name + "/" + analytics_key_file_name)
+
+    // delete and re-create directory
+    resetDir(secret_folder_name)
+    fs.writeFile(filePath, analytics_key_file_data, (err) => {
+        if (err) console.error({ message: "could not create analytics key file", error: err })
+        else console.log("analytics key file created successfuly!")
+    })
+}
+
+function resetDir(dirName) {
+    removeDir(dirName)
+    createDir(dirName)
+}
+function removeDir(dirName) {
+    fs.rmSync(dirName, { recursive: true, force: true })
+}
+function createDir(dirName) {
+    fs.mkdirSync(dirName, { recursive: true });
+    console.log(`Folder created: ${dirName}`);
 }
 
 
